d16bb487ed30a04511fe666e33edd28cd87b325b
reda1609 <ahmedredasorour12@gmail.com>
2024-03-11 14:42:12 +0200

    URDF made git status!

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -29,7 +29,6 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -37,5 +36,5 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
     ])
